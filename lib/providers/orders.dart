ee4d7fa985b1a13ea101e2661c312280008349ef
maka y <nord2sudjp@gmail.com>
2020-11-23 17:38:54 +0900

    attach users with orders

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -22,8 +22,9 @@ class OrderItem {
 class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
   final String authToken;
+  final String userId;
 
-  Orders(this.authToken, this._orders);
+  Orders(this.authToken, this.userId, this._orders);
 
   List<OrderItem> get orders {
     return [..._orders];
@@ -32,7 +33,7 @@ class Orders with ChangeNotifier {
   Future<void> fetchAndSetOrders() async {
     try {
       final url =
-          'https://fluttershopapp-63e96.firebaseio.com/orders.json?auth=$authToken';
+          'https://fluttershopapp-63e96.firebaseio.com/orders/$userId.json?auth=$authToken';
       final response = await http.get(url);
       final List<OrderItem> loadedOrders = [];
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -68,7 +69,7 @@ class Orders with ChangeNotifier {
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     final url =
-        'https://fluttershopapp-63e96.firebaseio.com/orders.json?auth=$authToken';
+        'https://fluttershopapp-63e96.firebaseio.com/orders/$userId.json?auth=$authToken';
     final timestamp = DateTime.now();
 
     try {
