95195795cac78564555f1b719fdfb20139e48853
maka y <nord2sudjp@gmail.com>
2020-11-23 14:40:04 +0900

    handling login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,10 +8,10 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    print("signup:call google");
-    const url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCCOvLUMJ9fhs9kvGE4DDWViequJf9eciI";
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyCCOvLUMJ9fhs9kvGE4DDWViequJf9eciI';
     final response = await http.post(
       url,
       body: json.encode(
@@ -23,5 +23,13 @@ class Auth with ChangeNotifier {
       ),
     );
     print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
